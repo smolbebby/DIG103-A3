78469f56f6a163ae9fba3298c4be44e54448faa2
smolbebby <leo.vandesande@outlook.com>
2024-04-17 20:43:46 +1000

    added the following: - JS file     - open and close MobileMenu functions     - query selector for MobileMenu - HTML file     - script (with defer)     - MobileMenu layout with JS functions - CSS file     - removed margin top from header     - created mobile-menu class

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Initialising");
+
+const mobileMenu = document.querySelector(".mobile-menu");
+
+function openMobileMenu() {
+  mobileMenu.style.display = "flex";
+}
+
+function closeMobileMenu() {
+  mobileMenu.style.display = "none";
+}
