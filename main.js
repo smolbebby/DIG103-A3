7643ed6797fb1c53baf95effce5f2de535979d99
smolbebby <leo.vandesande@outlook.com>
2024-04-25 15:13:18 +1000

    modified the following: - html     - added desktop stylesheet     - added minilogo to close mobile menu     - updated class name for logo     - added in main logo, tagline and dates content     - added on sale slider/divider (ish)     - added line up and locations content     - added footer content     - added youtube reference for on sale slider - css     - created desktop stylesheet     - added section class     - updated big-ticket-button height and width     - updated main logo     - added in unique styles for pre-existing classes     - added css for on sale slider - js     - added on-sale slider animation functions         - via querySelector and media queries         - forEach loop for duplicated items

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 console.log("Initialising");
 
+// HAMBURGER MENU - OPEN AND CLOSE MOBILE MENU
 const mobileMenu = document.querySelector(".mobile-menu");
+const body = document.querySelector("body");
 
 function openMobileMenu() {
   mobileMenu.style.display = "flex";
+  body.style.overflowY = "hidden";
 }
 
 function closeMobileMenu() {
   mobileMenu.style.display = "none";
+  body.style.overflowY = "scroll";
+}
+
+// ON-SALE SLIDER
+const scrollers = document.querySelectorAll(".scroller");
+
+// looking for media query on if user does not have reduced-motion turned on
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+// displays data animation (if user matches query)
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
 }
