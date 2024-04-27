fbb12432b3ea8f61fc4f43f63464edb454ad9659
smolbebby <leo.vandesande@outlook.com>
2024-04-27 15:42:46 +1000

    updated the following: - html     - re-created scroller - css     - re-created scroller classes     - fiddled with rem in translate (very little to no glitching, but best i can get) - js     - re-created scroller     - created mulitple duplications to fill screen without excess li in html file
    I FINALLY GOT THE SLIDER WORKING FOR ALL SCREEN SIZES I CAN DIE IN PEACE NOW!!!
    
    .. now to do other javascript stuff :sweat_smile:

--- main.js
+++ main.js
@@ -23,44 +23,53 @@ function closeMobileMenu() {
 //   desktopHeader.style.display = "flex";
 // }
 
-// TESTING MEDIA?
-const desktopHeader = document.querySelector(".desktop-header");
-const mobileHeader = document.querySelector(".header");
+// // TESTING MEDIA?
+// const desktopHeader = document.querySelector(".desktop-header");
+// const mobileHeader = document.querySelector(".header");
 
-function mobileToDesktop() {
-  window.matchMedia("(min-width: 750px").addEventListener(function (e) {
-    if (e.matches) {
-      desktopHeader.style.display = "flex";
-      mobileHeader.style.display = "none";
-    } else {
-      desktopHeader.style.display = "hidden";
-      mobileHeader.style.display = "flex";
-    }
-  });
-}
+// function mobileToDesktop() {
+//   window.matchMedia("(min-width: 750px").addEventListener(function (e) {
+//     if (e.matches) {
+//       desktopHeader.style.display = "flex";
+//       mobileHeader.style.display = "none";
+//     } else {
+//       desktopHeader.style.display = "hidden";
+//       mobileHeader.style.display = "flex";
+//     }
+//   });
+// }
 
 // ON-SALE SLIDER
 const scrollers = document.querySelectorAll(".scroller");
 
-// looking for media query on if user does not have reduced-motion turned on
+//if user does not have reduced motion, proceed with using animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
-// displays data animation (if user matches query)
 function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
 
-    // looking for scroller inner property in html, then sends array
-    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerInner = document.querySelector(".scroller__inner");
     const scrollerContent = Array.from(scrollerInner.children);
 
-    // forEach loop to duplicate array/list alongside hide duplicates from screenreaders
     scrollerContent.forEach((item) => {
       const duplicatedItem = item.cloneNode(true);
       duplicatedItem.setAttribute("aria-hidden", true);
       scrollerInner.appendChild(duplicatedItem);
+    });
+
+    scrollerContent.forEach((item) => {
+      const secondDuplication = item.cloneNode(true);
+      secondDuplication.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(secondDuplication);
+    });
+
+    scrollerContent.forEach((item) => {
+      const thirdDuplication = item.cloneNode(true);
+      thirdDuplication.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(thirdDuplication);
     });
   });
 }
